40b76a61a02021027c591c8082250d93f1725b54
Nhlanhla Percy Thwala <164434890+PRT27@users.noreply.github.com>
2024-12-11 05:25:13 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+// Add event listener to button
+document.querySelector('button').addEventListener('click', () => {
+    // Scroll to token info section
+    document.querySelector('.token-info').scrollIntoView({ behavior: 'smooth' });
+});
